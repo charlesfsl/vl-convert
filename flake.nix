292314d46be8fb33c279a793875f2542d17bb692
Charles Suggs <csuggs4@gmail.com>
2025-01-12 11:37:50 -0600

    Adds flake.nix to reproduce locally

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "vl-convert segfault local reproduction";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.11"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+  let
+    inherit (nixpkgs.lib) optional optionals;
+    pkgs = import nixpkgs { inherit system; };
+
+
+    locales = pkgs.glibcLocales;
+  in
+  with pkgs;
+  {
+    devShells.default = pkgs.mkShell {
+      buildInputs = [
+        locales
+        pkgs.cargo
+      ] ++ optional stdenv.isLinux inotify-tools
+      ++ optional stdenv.isDarwin terminal-notifier
+      ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+        CoreFoundation
+        CoreServices
+      ]);
+    };
+    packages.default = cargo;
+  });
+}
